0c88df47a0ec344e94074dbe1c7aa46f94633e08
Neo Phetaone <phetoaneneo4@gmail.com>
2025-06-10 12:53:13 +0200

    Colors and button altered.

--- src/components/SiteHero.jsx
+++ src/components/SiteHero.jsx
@@ -1,15 +1,19 @@
 import { Link } from "react-router-dom";
+import { ArrowRight } from "lucide-react";
 
 export default function SiteHero() {
   return (
     <section
-      className="relative bg-cover bg-center text-white text-center py-65 px-4 sm:px-8 md:px-20"
-      style={{ backgroundImage: "url('/sitehero.jpg')" }}
+      className="relative bg-cover bg-center text-white text-center py-55 px-4 sm:px-8 md:px-20"
+      style={{ backgroundImage: "url('/hero.jpg')" }}
     >
-      <div className="absolute inset-0 bg-black/60"></div>
+      <div className="absolute inset-0 bg-black/50"></div>
       <div className="relative z-10 max-w-4xl mx-auto flex flex-col items-center space-y-6">
         <h1 className="text-4xl md:text-6xl font-bold">
-          Welcome to IndabaX Equatorial Guinea
+          Welcome to{" "}
+          <span className="text-true-yellow font-semibold">
+            IndabaX Equatorial Guinea
+          </span>{" "}
         </h1>
         <p className="text-lg md:text-2xl">
           IndabaX Equatorial Guinea is a local chapter of the Deep Learning
@@ -20,9 +24,9 @@ export default function SiteHero() {
         <p className="text-lg md:text-2xl">Do not miss the next event: </p>
         <Link
           to="/registration"
-          className="bg-true-yellow hover:true-blue text-blue-900 font-bold py-6 px-6 rounded-md transition"
+          className="bg-true-blue hover:true-blue text-true-yellow text-xl font-semibold py-6 px-10 rounded-full transition animate-pulse-slow"
         >
-          Register Now
+          Register Now <ArrowRight className="inline" />
         </Link>
       </div>
     </section>
