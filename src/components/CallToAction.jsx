36d09a9e5fc9df74fa375cd5270f7975ac6f42bf
Neo Phetaone <phetoaneneo4@gmail.com>
2025-06-12 16:06:24 +0200

    Refactored CallToAction component layout: added container for improved structure and styling, enhanced text visibility, and adjusted background properties for better design.

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -3,18 +3,22 @@ import { Link } from "react-router-dom";
 export default function CallToAction() {
   return (
     <main>
-      <section className="bg-true-blue py-16 text-center">
-        <h2 className="text-3xl font-bold mb-4">Want to Get Involved?</h2>
-        <p className="text-lg mb-6 text-gray-700">
-          Whether you're a student, educator, or professional — join us in
-          shaping the future of AI in Africa.
-        </p>
-        <Link
-          to="/registration"
-          className="bg-true-blue hover:bg-yellow text-true-yellow font-semibold py-3 px-6 rounded-lg shadow-xl transition"
-        >
-          Register Now
-        </Link>
+      <section className="py-16 bg-transparent">
+        <div className="max-w-7xl mx-auto bg-true-blue border-4 border-yellow-100 rounded-full shadow-xl px-6 py-12 text-center">
+          <h2 className="text-3xl font-bold leading-tight text-white sm:text-4xl lg:text-5xl">
+            Want to Get Involved?
+          </h2>
+          <p className="text-lg mb-6 text-white">
+            Whether you're a student, educator, or professional — join us in
+            shaping the future of AI in Africa.
+          </p>
+          <Link
+            to="/registration"
+            className="bg-white hover:bg-yellow text-true-blue hover:text-true-blue font-semibold py-3 px-6 rounded-lg shadow transition"
+          >
+            Register Now
+          </Link>
+        </div>
       </section>
     </main>
   );
