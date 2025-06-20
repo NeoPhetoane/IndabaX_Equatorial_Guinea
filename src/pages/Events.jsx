5e220b3b4d5ae03aca6cf545724b19fb0126c7d7
Neo Phetaone <phetoaneneo4@gmail.com>
2025-06-20 11:32:46 +0200

    Fix typo in Events section and adjust layout for better consistency.

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -19,7 +19,7 @@ export default function Events() {
         <div className="relative">
           <h1 className="text-5xl md:text-6xl font-bold">Events</h1>
           <p className="mt-4 text-xl md:text-2xl">
-            See out past and future Events
+            See our past and future Events
           </p>
         </div>
       </section>
@@ -28,21 +28,13 @@ export default function Events() {
         style={{ backgroundImage: "url('/bgwhite.png')" }}
       >
         <div className="max-w-7xl mx-auto">
-          {/* <h2 className="text-3xl font-bold leading-tight text-gray-900 sm:text-4xl lg:text-5xl text-center mb-10">
-            Events
-          </h2> */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-10">
             {eventsData.map((event) => (
               <Link
                 key={event.id}
                 to={`/events/${event.id}`}
-                className="group relative bg-white shadow-xl hover:shadow-2xl transition-shadow duration-300 rounded-2xl overflow-hidden border border-gray-200"
+                className="group relative bg-white shadow-xl hover:shadow-2xl transition-shadow duration-300 rounded-lg overflow-hidden border border-gray-200"
               >
-                <div
-                  className="absolute left-0 top-0 h-full w-1.5 rounded-r-full"
-                  style={{ backgroundColor: `var(--tw-${event.barColor})` }}
-                ></div>
-
                 <div
                   className="h-48 w-full bg-cover bg-center"
                   style={{ backgroundImage: `url(${event.image})` }}
@@ -53,18 +45,6 @@ export default function Events() {
                     {event.icon} {event.title}
                   </h3>
                   <p className="text-gray-600 mb-3">{event.description}</p>
-                  {/* <p className="text-sm text-gray-500">
-                    <strong>Date:</strong> {event.date}
-                  </p>
-                  <p className="text-sm text-gray-500">
-                    <strong>Time:</strong> {event.time}
-                  </p>
-                  <p className="text-sm text-gray-500">
-                    <strong>Location:</strong> {event.location}
-                  </p>
-                  <p className="text-sm text-gray-500">
-                    <strong>Organizer:</strong> {event.organizer}
-                  </p> */}
                   <div className="mt-4 flex space-x-3">
                     <Link
                       key={event.id}
